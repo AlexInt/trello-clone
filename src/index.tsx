004923473fe8755cff72a4f6145530a8e0458bef
Jimmy <veniveci@aliyun.com>
2023-03-29 12:00:54 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';  //v18
+// import ReactDOM from 'react-dom'; //v17
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+/*v18*/
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,6 +15,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+/*v17*/
+// const root = document.getElementById('root')!;
+// ReactDOM.render(<App />, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
